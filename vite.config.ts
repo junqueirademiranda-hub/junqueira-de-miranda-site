d232d76f0c86205319f7685cef9024cc06c2c9e1
junqueirademiranda-hub <junqueirademiranda@gmail.com>
2026-08-10 16:49:55 -0400

    Update vite.config.ts
    Configurar banco D1 do portal

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "88de487d-7ef8-450e-9291-5f5c65aa4749";
 
 const { d1, r2 } = hostingConfig;
 
@@ -18,7 +18,7 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: "site-creator-d1",
+          database_name: "junqueira-portal-db",
           database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
         },
       ]
